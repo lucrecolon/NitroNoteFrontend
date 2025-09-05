57aa0a2af30a5bb0b7e5a700de47b47aa3820f2f
Fabricio <fachucamilla32@gmail.com>
2025-09-05 17:20:34 -0300

    ADD: dependencia axios

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Api from '../service.js';
+
 import { View, Text, Button } from 'react-native';
 
 export default function LoginScreen({ navigation }) {
+  
+  const products = ()=>{
+    Api.all_vehiculo().then(response => {
+      console.log(response);
+    })
+  }
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Login / Registro</Text>
-      <Button title="Ingresar" onPress={() => navigation.replace('MainTabs')} />
+      <Button title="Ingresar" onPress={() => products()} />
     </View>
   );
 }
---
+++ src/service/service.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+
+const api_base_url = 'http://192.168.19.90:7070';
+
+const api_endpoints = {
+    vehiculo: `${api_base_url}/vehiculo`
+}
+
+const all_vehiculos = async () => {
+
+    try{
+        const vehiculos = await axios.get(`${api_base_url.vehiculo}`)
+        console.log(vehiculos)
+        return vehiculos
+    }
+    catch(e){
+        return Promise.reject(e);
+    }
+};
+
+const Api = {
+    all_vehiculos
+}
+
+export default Api;
