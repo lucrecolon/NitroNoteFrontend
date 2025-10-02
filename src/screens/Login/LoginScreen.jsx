a5303cb9d6f69d851d47894db4b89a3a6c96e9b2
Leandro <leandropeppe@gmail.com>
2025-10-02 15:26:38 -0300

    Se agrego el showpassword para mostrar/ocultar la contraseña al loguearse

--- src/screens/Login/LoginScreen.jsx
+++ src/screens/Login/LoginScreen.jsx
@@ -1,14 +1,16 @@
 import { useState, useContext } from "react";
-import { Text, TextInput, TouchableOpacity } from "react-native";
+import {Text, TextInput, TouchableOpacity, View} from "react-native";
 import { AuthContext } from "../../hooks/AuthContext";
 import Toast from "react-native-toast-message";
 import Api from "../../service/service";
+import {Ionicons} from "@expo/vector-icons";
 
 export default function LoginScreen({ navigation }) {
     
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const {setUser} = useContext(AuthContext);
+    const [showPassword, setShowPassword] = useState(false);
     
     const handleLogin = async ()  => {
         try{
@@ -35,12 +37,20 @@ export default function LoginScreen({ navigation }) {
                 style={styles.input}
                 keyboardType="email-address"
             />
-            <TextInput
-                placeholder="Contraseña"
-                value={password}
-                onChangeText={setPassword}
-                style={styles.input}
-            />
+
+            <View style={styles.inputPassword}>
+                <TextInput
+                    placeholder="Contraseña"
+                    style={styles.inputText}
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry={!showPassword}
+                />
+                <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.eyeButton}>
+                    <Ionicons name={showPassword ? "eye-off" : "eye"} size={22} color="#666" />
+                </TouchableOpacity>
+            </View>
+
             <TouchableOpacity style={styles.button} onPress={handleLogin}>
                 <Text style={{ color: "white", fontWeight: "600", textAlign: "center" }}>
                     Iniciar Sesión
@@ -51,21 +61,41 @@ export default function LoginScreen({ navigation }) {
 }
 
 const styles = {
-  input: {
-    borderWidth: 1,
-    borderColor: "#d1d5db",
-    borderRadius: 10,
-    margin:10,
-    left: 10,
-    width:335,
-    backgroundColor: "#f9fafb",
-  },
-  button: {
-    backgroundColor: "#2563eb",
-    padding: 15,
-    width:335,
-    left: 20,
-    borderRadius: 10,
-    marginTop: 10,
-  },
+    input: {
+        borderWidth: 1,
+        borderColor: "#d1d5db",
+        borderRadius: 10,
+        margin: 10,
+        left: 10,
+        width: 335,
+        backgroundColor: "#f9fafb",
+        padding: 10,
+    },
+    inputPassword: {
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "#d1d5db",
+        borderRadius: 10,
+        margin: 10,
+        left: 10,
+        width: 335,
+        backgroundColor: "#f9fafb",
+        paddingRight: 10, // espacio para el ojito
+    },
+    inputText: {
+        flex: 1,
+        padding: 10,
+    },
+    button: {
+        backgroundColor: "#2563eb",
+        padding: 15,
+        width: 335,
+        left: 20,
+        borderRadius: 10,
+        marginTop: 10,
+    },
+    eyeButton: {
+        paddingLeft: 8,
+    },
 };
