6ec4839afa7c81fa5b5f783679e5f6f1462c35bd
lucrecolon <lucrecolon1828@gmail.com>
2025-10-17 22:01:32 -0300

    intento de apk #1

--- src/service/service.js
+++ src/service/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const api_base_url = 'http://192.168.1.92:8080';
+const api_base_url = 'https://nitronotebackend-production.up.railway.app/';
 
 const getConfig = async () => {
     const token = await AsyncStorage.getItem('token');
